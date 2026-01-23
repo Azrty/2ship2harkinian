92b9dbd6b3067b2e16b636b80f0f549918d00039
inspectredc <78732756+inspectredc@users.noreply.github.com>
2026-01-23 18:43:22 +0000

    1.0 extracting

--- mm/2s2h/BenPort.cpp
+++ mm/2s2h/BenPort.cpp
@@ -621,7 +621,7 @@ void OTRGlobals::Initialize() {
         context->GetResourceManager()->GetArchiveManager()->AddArchive(mmPath);
     }
 
-    std::unordered_set<uint32_t> validHashes = { MM_NTSC_US_10, MM_NTSC_US_GC, MM_PAL_GC, MM_PAL_11 };
+    std::unordered_set<uint32_t> validHashes = { MM_NTSC_US_10, MM_NTSC_US_GC, MM_PAL_10, MM_PAL_11, MM_PAL_GC };
 
 #if (_DEBUG)
     auto defaultLogLevel = spdlog::level::trace;
@@ -1318,6 +1318,7 @@ extern "C" uint32_t ResourceMgr_GetGamePlatform(int index) {
 
     switch (version) {
         case MM_NTSC_US_10:
+        case MM_PAL_10:
         case MM_PAL_11:
             return GAME_PLATFORM_N64;
         case MM_NTSC_US_GC:
@@ -1334,6 +1335,7 @@ extern "C" uint32_t ResourceMgr_GetGameRegion(int index) {
         case MM_NTSC_US_10:
         case MM_NTSC_US_GC:
             return GAME_REGION_NTSC;
+        case MM_PAL_10:
         case MM_PAL_11:
         case MM_PAL_GC:
             return GAME_REGION_PAL;
